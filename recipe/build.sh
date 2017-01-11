1155f925229eb5b8aa019354ceb0fada3e4a87e1
Julien Schueller <schueller@phimeca.com>
2017-01-11 12:07:42 +0100

    Skip build rpath

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,7 @@ mkdir build && cd build
 cmake \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+  -DCMAKE_SKIP_BUILD_RPATH=TRUE \
   -DBUILD_SWIG_JAVA=OFF \
   -DBUILD_TESTS=OFF \
   ..
